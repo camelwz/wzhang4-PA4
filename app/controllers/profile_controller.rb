3bb5e1087c07444120d9e7da1e0fe2897f50e9f9
camelwz <wzhang4@conncoll.edu>
2025-04-16 16:46:43 -0400

    first commit

---
+++ app/controllers/profile_controller.rb
@@ -0,0 +1,18 @@
+class ProfileController < ApplicationController
+
+    def index
+    end
+
+    def about
+    end
+
+    def contact
+    end
+
+    def portfolio
+    end
+
+    def gallery 
+    end
+
+end
